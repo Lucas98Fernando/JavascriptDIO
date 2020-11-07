696cd7d733d36dea65b2612356e9007ab21edbdd
Lucas <lucas98fernando@gmail.com>
2020-11-07 12:45:12 -0300

    Arrays e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* ===== Aula 1 - Strings, alerts e console  =======
+
 var nome = "Lucas Fernando";
 var idade = 22;
 
@@ -26,3 +28,48 @@ console.log(texto.toLowerCase());
 function apresentacao() {
     alert("Meu nome é " + nome + ", tenho " + idade + " anos.");
 }
+
+===== Fim do conteúdo da aula 1 ===== */
+
+/* ======= Aula 2 - Array =========
+// O array sempre começa com o índice 0
+// Array de frutas
+var frutas = ["Maçã", "Laranja", "Morango", "Abacaxi"];
+// Exibe como saída no console apenas Maçã
+// console.log(frutas[0]);
+
+// O psuh serve para inserir novos elementos no array
+frutas.push("Uva");
+console.log(frutas);
+
+// O pop serve para remover o último elementos no array
+frutas.pop();
+console.log(frutas);
+
+// O length serve para medir o tamanho do array
+console.log(frutas.length);
+
+// O reverse serve para inverter a ordem dos elementos dentro do array
+console.log(frutas.reverse());
+
+// O toString converte todos os elementos do array para uma String
+console.log(frutas.toString());
+
+// O join altera os elementos de separação entre os elementos dentro do array
+console.log(frutas.join(" - "));
+
+===== Fim do conteúdo sobre arrays */
+
+
+// Dicionário de arrays
+var frutas = [{
+    nome: "Maçã",
+    cor: "Verde"
+}, 
+{
+    nome: "Melancia",
+    cor: "Vermelha"
+}];
+
+// Exibe como saída Vermelha
+console.log(frutas[1].cor);
